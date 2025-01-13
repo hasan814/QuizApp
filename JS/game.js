853f230c778c892b6552961dd090e43339109ac1
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-13 22:28:15 +0330

    Added ShowQuestion Function

--- JS/game.js
+++ JS/game.js
@@ -1,12 +1,16 @@
-import { formatData } from "./helper.js";
+import { checkAnswer, formatData } from "./helper.js";
 
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const answerList = document.querySelectorAll(".answer-list");
+const questionText = document.getElementById("question-text");
 
+let correctAnswer;
 let formattedData;
+let questionIndex = 0;
 
 const fetchData = async () => {
   const response = await fetch(URL);
@@ -16,8 +20,22 @@ const fetchData = async () => {
 };
 
 const start = () => {
+  showQuestion();
   loader.style.display = "none";
   container.style.display = "block";
 };
 
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((btn, index) => {
+    btn.innerText = answers[index];
+  });
+};
+
 window.addEventListener("load", fetchData);
+answerList.forEach((btn, index) => {
+  btn.addEventListener("click", checkAnswer);
+});
--- JS/helper.js
+++ JS/helper.js
@@ -13,3 +13,5 @@ export const formatData = (questionData) => {
     };
   });
 };
+
+export const checkAnswer = () => {};
