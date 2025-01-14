a9b12a28b59c7b961df861c6bc6c1f315429d68d
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-14 12:11:20 +0330

    Show HighScores

---
+++ JS/scores.js
@@ -0,0 +1,15 @@
+const list = document.querySelector("ol");
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const content = highScores.map((score, index) => {
+  return `
+  <li>
+    <span>${index + 1}</span>
+    <p>${score.name}</p>
+    <span>${score.score}</span>
+  </li>
+  `;
+});
+
+list.innerHTML = content.join("");
