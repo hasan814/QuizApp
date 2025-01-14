dfa744cb82d7717334d593454ce57f0f8055d92a
Hasan Moosavi <h.mousavi910@gmail.com>
2025-01-14 09:42:28 +0330

    Added Correct_Bonus

--- JS/game.js
+++ JS/game.js
@@ -4,13 +4,18 @@ const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
 const loader = document.getElementById("loader");
+const scoreText = document.getElementById("score");
 const container = document.getElementById("container");
 const answerList = document.querySelectorAll(".answer-list");
 const questionText = document.getElementById("question-text");
 
+let score = 0;
 let correctAnswer;
 let formattedData;
+let isAccepted = true;
 let questionIndex = 0;
+
+const CORRECT_BONUS = 10;
 
 const fetchData = async () => {
   const response = await fetch(URL);
@@ -37,8 +42,13 @@ const showQuestion = () => {
 };
 
 const checkAnswer = (event, index) => {
+  if (!isAccepted) return;
+  isAccepted = false;
   const isCorrect = index === correctAnswer ? true : false;
-  if (isCorrect) event.target.classList.add("correct");
+  if (isCorrect)
+    event.target.classList.add("correct"),
+      (score += CORRECT_BONUS),
+      (scoreText.innerText = score);
   else
     event.target.classList.add("incorrect"),
       answerList[correctAnswer].classList.add("correct");
